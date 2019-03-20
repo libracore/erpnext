9fa81f9c07bfe487ecc7b34dce557434547a8457
lasalesi <lars.mueller@libracore.com>
2019-03-20 21:17:11 +0100

    new sales person parameter

--- erpnext/selling/doctype/sales_report/sales_report.py
+++ erpnext/selling/doctype/sales_report/sales_report.py
@@ -19,14 +19,16 @@ class SalesReport(Document):
         
     """ This function is called in a script fashion to populate the report 
         It can be executed from the wrapper
-            myapp.myapp.doctype.sales_report.sales_report.create_report()
+            erpnext.selling.doctype.sales_report.sales_report.create_report
     """
-    def fill(self):
+    def fill(self, sales_person):
         # prepare global variables
         today = datetime.now()
         self.date = today.strftime("%Y-%m-%d")
         self.week = today.strftime("%W")
         self.title = "Sales Report " + str(self.date)
+        if sales_person != "%":
+            self.title += " " + sales_person
         _week = int(today.strftime("%W"))
         _this_year = int(today.strftime("%Y"))
         _last_year = _this_year - 1
@@ -61,51 +63,54 @@ class SalesReport(Document):
 
         for line_item in line_items:
             _description = line_item['description']
-            _sql_7days = """SELECT (IFNULL(SUM(`kg`), 0)) AS `qty`, 
+            _sql_7days = """SELECT (IFNULL(SUM(`kg`), 0)) AS `qty`,
                     (IFNULL(SUM(`net_amount`), 0)) AS `revenue`
                 FROM `viewDelivery`
-                WHERE ({0})
+                WHERE ({filter})
                 AND `docstatus` = 1
                 AND `posting_date` > DATE_SUB(NOW(), INTERVAL 7 DAY)
-                """.format(line_item['filter'])
+                AND `sales_person` LIKE '{sales_person}'
+                """.format(filter=line_item['filter'], sales_person=sales_person)
             _qty_7days,_revenue_7days = get_qty_revenue(_sql_7days)
-            _sql_YTD = """SELECT (IFNULL(SUM(`kg`), 0)) AS `qty`, 
+            _sql_YTD = """SELECT (IFNULL(SUM(`kg`), 0)) AS `qty`,
                     (IFNULL(SUM(`net_amount`), 0)) AS `revenue`
                 FROM `viewDelivery`
-                WHERE ({1})
+                WHERE ({filter})
                 AND `docstatus` = 1
-                AND `posting_date` >= '{0}-01-01'
-                """.format(today.strftime("%Y"), line_item['filter'])
+                AND `posting_date` >= '{year}-01-01'
+                AND `sales_person` LIKE '{sales_person}'
+                """.format(year=today.strftime("%Y"), filter=line_item['filter'], sales_person=sales_person)
             _qty_YTD,_revenue_YTD = get_qty_revenue(_sql_YTD)
-            _sql_PY = """SELECT (IFNULL(SUM(`kg`), 0)) AS `qty`, 
+            _sql_PY = """SELECT (IFNULL(SUM(`kg`), 0)) AS `qty`,
                     (IFNULL(SUM(`net_amount`), 0)) AS `revenue`
                 FROM `viewDelivery`
-                WHERE ({1})
+                WHERE ({filter})
                 AND `docstatus` = 1
-                AND `posting_date` >= '{0}-01-01'
-                AND `posting_date` <= '{0}-12-31'
-                """.format(int(today.strftime("%Y")) - 1, line_item['filter'])
+                AND `posting_date` >= '{year}-01-01'
+                AND `posting_date` <= '{year}-12-31'
+                AND `sales_person` LIKE '{sales_person}'
+                """.format(year=int(today.strftime("%Y")) - 1, filter=line_item['filter'], sales_person=sales_person)
             _qty_PY,_revenue_PY = get_qty_revenue(_sql_PY)
-                            
-            self.append('items', 
-                { 
-                    'description': _description, 
+
+            self.append('items',
+                {
+                    'description': _description,
                     'qty_7days': _qty_7days,
                     'revenue_7days': _revenue_7days,
                     'qty_ytd': _qty_YTD,
-                    'revenue_ytd': _revenue_YTD, 
+                    'revenue_ytd': _revenue_YTD,
                     'qty_py': _qty_PY,
                     'revenue_py': _revenue_PY,
                     'demand_qty_ytd': (_qty_YTD/_week),
                     'demand_revenue_ytd': (_revenue_YTD/_week),
-                    'demand_qty_py': (_qty_PY/52), 
+                    'demand_qty_py': (_qty_PY/52),
                     'demand_revenue_py': (_revenue_PY/52)
                 })
-        
+
         # compute totals
         self.update_totals()
         return
-    
+
     def update_totals(self):
         _total_qty_7days = 0
         _total_qty_ytd = 0 
@@ -133,7 +138,7 @@ class SalesReport(Document):
         except:
             pass
         self.total_qty_7days = _total_qty_7days
-        self.total_qty_ytd = _total_qty_ytd 
+        self.total_qty_ytd = _total_qty_ytd
         self.total_demand_qty_ytd = _total_demand_qty_ytd
         self.total_demand_qty_py = _total_demand_qty_py
         self.total_qty_py = _total_qty_py
@@ -141,15 +146,15 @@ class SalesReport(Document):
         self.total_revenue_ytd = _total_revenue_ytd
         self.total_demand_revenue_ytd = _total_demand_revenue_ytd
         self.total_demand_revenue_py = _total_demand_revenue_py
-        self.total_revenue_py = _total_revenue_py     
-        return   
-        
+        self.total_revenue_py = _total_revenue_py
+        return
+
 @frappe.whitelist()
-def create_report():
+def create_report(sales_person="%"):
     new_report = frappe.get_doc({"doctype": "Sales Report"})
-    new_report.fill()
-    new_report_name = new_report.insert()        
-    return new_report
+    new_report.fill(sales_person)
+    new_report_name = new_report.insert()
+    return
 
 """ Extracts `result` from SQL query """
 def get_value(sql):
