a1aa6c6e6ee8a3d1bb6e6bdc0e0398fcfba2ea99
lasalesi <larsericmueller@gmail.com>
2019-09-13 21:35:07 +0200

    different role creator (implicit error on migrate)

---
+++ erpnext/starter/doctype_simplifier.py
@@ -0,0 +1,8 @@
+# Copyright 2019 libracore and contributors
+#
+# Simplification model for DocTypes
+# 
+# Extension through patches to maintain local changes
+
+import frappe
+
--- erpnext/starter/role_deployment.py
+++ erpnext/starter/role_deployment.py
@@ -5,16 +5,13 @@
 import frappe
 
 def create_roles():
-    check_create_role("libracore User")
-    check_create_role("libracore Manager")
+    check_create_role("Starter User")
+    check_create_role("Starter Manager")
         
 def check_create_role(role):
     if not frappe.db.exists("Role", dict(role_name=role)): 
         # create role
-        frappe.get_doc({
-            "doctype": "Role",
-            "role_name": role,
-            "disabled": 0,
-            "desk_access": 1,
-            "two_factor_auth": 0
-        }).insert(ignore_permissions=True)
+        frappe.db.sql("""INSERT INTO `tabRole` 
+            (`name`, `role_name`, `disabled`, `desk_access`, `two_factor_auth`)
+            VALUES ('{role}', '{role}', 0, 1, 0);""")
+    return
