9930f7f3a8ece74daaa49c0128f7779ca98af619
lasalesi <larsericmueller@gmail.com>
2019-09-13 21:22:38 +0200

    improved role deployment

--- erpnext/starter/role_deployment.py
+++ erpnext/starter/role_deployment.py
@@ -9,15 +9,12 @@ def create_roles():
     check_create_role("Starter Manager")
         
 def check_create_role(role):
-    # check if the role already exists
-    r = frappe.get_all("Role", filters={'role_name': role}, fields=['role_name'])
-    if not r: 
+    if not frappe.db.exists("Role", role): 
         # create role
-        new_role = frappe.get_doc({
+        frappe.get_doc({
             "doctype": "Role",
             "role_name": role,
             "disabled": 0,
             "desk_access": 1,
             "two_factor_auth": 0
-        })
-        new_role.insert()
+        }).insert(ignore_permissions=True)
