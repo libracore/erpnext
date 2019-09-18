4c6248775e0c1e40cd5cfe1b91534130fa0f6d01
lasalesi <larsericmueller@gmail.com>
2019-09-18 16:46:16 +0200

    permission refinement

--- erpnext/starter/doctype_simplifier.py
+++ erpnext/starter/doctype_simplifier.py
@@ -16,4 +16,14 @@ def hide_fields(doctype, fieldname, hidden=1):
     
 def initialise_simple_doctypes():
     hide_fields("Customer", "pan")
+    hide_fields("Customer", "from_lead")
+    hide_fields("Customer", "default_bank_account")
+    hide_fields("Customer", "tax_category")
+    hide_fields("Customer", "is_internal_customer")
+    hide_fields("Customer", "column_break_38") # locality points section
+    hide_fields("Customer", "sales_team_section_break")
+    hide_fields("Customer", "sales_team_section")
+    hide_fields("Customer", "account_manager")
+    hide_fields("Customer", "is_frozen")
+    
     return
