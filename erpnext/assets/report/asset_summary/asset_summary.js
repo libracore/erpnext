07c6f7b8074b9b5976d0f3a46ffa59b6eb3dc3e6
lasalesi <larsericmueller@gmail.com>
2021-01-05 21:07:54 +0100

    advanced asset overview

--- erpnext/assets/report/asset_summary/asset_summary.js
+++ erpnext/assets/report/asset_summary/asset_summary.js
@@ -3,34 +3,39 @@
 /* eslint-disable */
 
 frappe.query_reports["Asset Summary"] = {
-	"filters": [
-		{
-			"fieldname":"company",
-			"label": __("Company"),
-			"fieldtype": "Link",
-			"options": "Company",
-			"default": frappe.defaults.get_user_default("Company"),
-			"reqd": 1
-		},
-		{
-			"fieldname":"from_date",
-			"label": __("From Date"),
-			"fieldtype": "Date",
-			"default": frappe.defaults.get_user_default("year_start_date"),
-			"reqd": 1
-		},
-		{
-			"fieldname":"to_date",
-			"label": __("To Date"),
-			"fieldtype": "Date",
-			"default": frappe.defaults.get_user_default("year_end_date"),
-			"reqd": 1
-		},
-		{
-			"fieldname":"asset_category",
-			"label": __("Asset Category"),
-			"fieldtype": "Link",
-			"options": "Asset Category"
-		}
-	]
+    "filters": [
+        {
+            "fieldname":"company",
+            "label": __("Company"),
+            "fieldtype": "Link",
+            "options": "Company",
+            "default": frappe.defaults.get_user_default("Company"),
+            "reqd": 1
+        },
+        {
+            "fieldname":"from_date",
+            "label": __("From Date"),
+            "fieldtype": "Date",
+            "default": frappe.defaults.get_user_default("year_start_date"),
+            "reqd": 1
+        },
+        {
+            "fieldname":"to_date",
+            "label": __("To Date"),
+            "fieldtype": "Date",
+            "default": frappe.defaults.get_user_default("year_end_date"),
+            "reqd": 1
+        },
+        {
+            "fieldname":"asset_category",
+            "label": __("Asset Category"),
+            "fieldtype": "Link",
+            "options": "Asset Category"
+        },
+        {
+            "fieldname":"show_sums",
+            "label": __("Show Sums"),
+            "fieldtype": "Check"
+        }
+    ]
 };
