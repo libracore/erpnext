024f8de665a2701162d6de751b8c2aa04e0e7888
Joelios <joel.mesmer@libracore.com>
2021-06-30 16:29:57 +0200

    blanket order: new method to calc fulfilled_qty

--- erpnext/manufacturing/doctype/blanket_order/blanket_order.py
+++ erpnext/manufacturing/doctype/blanket_order/blanket_order.py
@@ -13,29 +13,35 @@ from erpnext.stock.doctype.item.item import get_item_defaults
 class BlanketOrder(Document):
     def update_ordered_qty(self):
         if self.blanket_order_type == "Selling":
-            ref_doctype = "Sales Order"
-            item_ordered_qty = frappe._dict(frappe.db.sql("""
-                select trans_item.item_code, sum(trans_item.stock_qty) as qty
-                from `tab{0} Item` trans_item, `tab{0}` trans
-                where trans.name = trans_item.parent
-                    and trans_item.blanket_order=%s
-                    and trans.docstatus=1
-                    and trans.status not in ('Closed', 'Stopped')
-                group by trans_item.item_code
-            """.format(ref_doctype), self.name))
-            item_delivered_qty = frappe._dict(frappe.db.sql("""
-                select trans_item.item_code, sum(trans_item.delivered_qty) as qty
-                from `tab{0} Item` trans_item, `tab{0}` trans
-                where trans.name = trans_item.parent
-                    and trans_item.blanket_order=%s
-                    and trans.docstatus=1
-                group by trans_item.item_code
-            """.format(ref_doctype), self.name))
             for d in self.items:
-                if item_ordered_qty.get(d.item_code, 0) < item_delivered_qty.get(d.item_code, 0):
-                    d.db_set("ordered_qty", item_delivered_qty.get(d.item_code, 0))
+                item_query = frappe.db.sql("""SELECT
+                                                    SUM(`map`.`fulfilled_qty`) AS `fulfilled_qty`
+                                                FROM
+                                                 (SELECT
+                                                      (CASE WHEN `so`.`status` NOT IN ('Closed', 'Stopped') AND SUM(`soi`.`stock_qty`) > SUM(`soi`.`delivered_qty`)
+                                                        THEN SUM(`soi`.`stock_qty`)
+                                                        ELSE SUM(`soi`.`delivered_qty`)
+                                                      END) AS `fulfilled_qty`,
+                                                      `bo`.`name` AS `blanket_order`
+                                                    FROM
+                                                      `tabSales Order` AS `so`
+                                                      INNER JOIN `tabSales Order Item` AS `soi` ON `soi`.`parent` = `so`.`name`
+                                                      LEFT JOIN (
+                                                        `tabBlanket Order Item` AS `boi` INNER JOIN `tabBlanket Order` AS `bo` ON `bo`.`name` = `boi`.`parent`
+                                                      ) ON `boi`.`item_code` = `soi`.`item_code` AND `bo`.`name` = `soi`.`blanket_order` AND `bo`.`docstatus` = 1
+                                                    WHERE
+                                                      `soi`.`item_code` = '{item_code}' AND
+                                                      `so`.`docstatus` = 1
+                                                      AND `bo`.`name` = '{blanket_order}'
+                                                    GROUP BY
+                                                      `so`.`name`
+                                                    ORDER BY
+                                                      `so`.`transaction_date`) AS `map`""".format(item_code=d.item_code, blanket_order=self.name), as_dict=True)
+                if item_query[0].fulfilled_qty:
+                    d.db_set("ordered_qty", item_query[0].fulfilled_qty)
                 else:
-                    d.db_set("ordered_qty", item_ordered_qty.get(d.item_code, 0))
+                    d.db_set("ordered_qty", 0)
+      
         else:
             ref_doctype = "Purchase Order"
             item_ordered_qty = frappe._dict(frappe.db.sql("""
