ea30525ca697baae6b954b7a3b79c0563a8f11d2
lasalesi <larsericmueller@gmail.com>
2021-11-29 11:05:21 +0100

    improvement: allow to remove standard filter quotation_to

--- erpnext/selling/doctype/quotation/quotation_list.js
+++ erpnext/selling/doctype/quotation/quotation_list.js
@@ -1,28 +1,30 @@
 frappe.listview_settings['Quotation'] = {
-	add_fields: ["customer_name", "base_grand_total", "status",
-		"company", "currency", 'valid_till'],
+    add_fields: ["customer_name", "base_grand_total", "status",
+        "company", "currency", 'valid_till'],
 
-	onload: function(listview) {
-		listview.page.fields_dict.quotation_to.get_query = function() {
-			return {
-				"filters": {
-					"name": ["in", ["Customer", "Lead"]],
-				}
-			};
-		};
-	},
+    onload: function(listview) {
+        if (listview.page.fields_dict.quotation_to) {
+            listview.page.fields_dict.quotation_to.get_query = function() {
+                return {
+                    "filters": {
+                        "name": ["in", ["Customer", "Lead"]],
+                    }
+                };
+            };
+        }
+    },
 
-	get_indicator: function(doc) {
-		if(doc.status==="Open") {
-			if (doc.valid_till && doc.valid_till < frappe.datetime.nowdate()) {
-				return [__("Expired"), "darkgrey", "valid_till,<," + frappe.datetime.nowdate()];
-			} else {
-				return [__("Open"), "orange", "status,=,Open"];
-			}
-		} else if(doc.status==="Ordered") {
-			return [__("Ordered"), "green", "status,=,Ordered"];
-		} else if(doc.status==="Lost") {
-			return [__("Lost"), "darkgrey", "status,=,Lost"];
-		}
-	}
+    get_indicator: function(doc) {
+        if(doc.status==="Open") {
+            if (doc.valid_till && doc.valid_till < frappe.datetime.nowdate()) {
+                return [__("Expired"), "darkgrey", "valid_till,<," + frappe.datetime.nowdate()];
+            } else {
+                return [__("Open"), "orange", "status,=,Open"];
+            }
+        } else if(doc.status==="Ordered") {
+            return [__("Ordered"), "green", "status,=,Ordered"];
+        } else if(doc.status==="Lost") {
+            return [__("Lost"), "darkgrey", "status,=,Lost"];
+        }
+    }
 };
