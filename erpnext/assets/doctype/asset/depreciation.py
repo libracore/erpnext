36174d91045d75f2c51dfa09a6bcbb888518bcef
lasalesi <larsericmueller@gmail.com>
2022-01-27 16:24:08 +0100

    new feature: date disposal of assets

--- erpnext/assets/doctype/asset/depreciation.py
+++ erpnext/assets/doctype/asset/depreciation.py
@@ -129,7 +129,7 @@ def get_depreciation_accounts(asset):
 	return fixed_asset_account, accumulated_depreciation_account, depreciation_expense_account
 
 @frappe.whitelist()
-def scrap_asset(asset_name):
+def scrap_asset(asset_name, date=None):
 	asset = frappe.get_doc("Asset", asset_name)
 
 	if asset.docstatus != 1:
@@ -142,7 +142,7 @@ def scrap_asset(asset_name):
 	je = frappe.new_doc("Journal Entry")
 	je.voucher_type = "Journal Entry"
 	je.naming_series = depreciation_series
-	je.posting_date = today()
+	je.posting_date = date or today()
 	je.company = asset.company
 	je.remark = "Scrap Entry for asset {0}".format(asset_name)
 
@@ -156,7 +156,7 @@ def scrap_asset(asset_name):
 	je.flags.ignore_permissions = True
 	je.submit()
 
-	frappe.db.set_value("Asset", asset_name, "disposal_date", today())
+	frappe.db.set_value("Asset", asset_name, "disposal_date", date or today())
 	frappe.db.set_value("Asset", asset_name, "journal_entry_for_scrap", je.name)
 	asset.set_status("Scrapped")
 
