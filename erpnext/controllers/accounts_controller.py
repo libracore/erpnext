7fad22da96c6bf961e4645b52dcf105add9d0d25
lasalesi <larsericmueller@gmail.com>
2024-06-11 16:42:13 +0200

    prevent overbilling warning on free items

--- erpnext/controllers/accounts_controller.py
+++ erpnext/controllers/accounts_controller.py
@@ -586,6 +586,8 @@ class AccountsController(TransactionBase):
 			if item.get(item_ref_dn):
 				ref_amt = flt(frappe.db.get_value(ref_dt + " Item",
 					item.get(item_ref_dn), based_on), self.precision(based_on, item))
+				if ref_amt == 0 and ref_amt == item.amount:
+					continue            # in case both reference and item are free, skip overbilling check
 				if not ref_amt:
 					frappe.msgprint(
 						_("Warning: System will not check overbilling since amount for Item {0} in {1} is zero")
