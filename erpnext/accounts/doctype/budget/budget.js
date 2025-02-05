dc21a5adf6b5abc6bd14843a2598573fb457bffe
lasalesi <larsericmueller@gmail.com>
2025-02-05 22:01:19 +0100

    adding totals to budget and monthly distribution

--- erpnext/accounts/doctype/budget/budget.js
+++ erpnext/accounts/doctype/budget/budget.js
@@ -1,62 +1,78 @@
-// Copyright (c) 2016, Frappe Technologies Pvt. Ltd. and contributors
+// Copyright (c) 2016-2025, libracore and contributors
 // For license information, please see license.txt
 
 frappe.ui.form.on('Budget', {
-	onload: function(frm) {
-		frm.set_query("cost_center", function() {
-			return {
-				filters: {
-					company: frm.doc.company
-				}
-			}
-		})
+    onload: function(frm) {
+        frm.set_query("cost_center", function() {
+            return {
+                filters: {
+                    company: frm.doc.company
+                }
+            }
+        })
 
-		frm.set_query("project", function() {
-			return {
-				filters: {
-					company: frm.doc.company
-				}
-			}
-		})
-		
-		frm.set_query("account", "accounts", function() {
-			return {
-				filters: {
-					company: frm.doc.company,
-					report_type: "Profit and Loss",
-					is_group: 0
-				}
-			}
-		})
-		
-		frm.set_query("monthly_distribution", function() {
-			return {
-				filters: {
-					fiscal_year: frm.doc.fiscal_year
-				}
-			}
-		})
-	},
+        frm.set_query("project", function() {
+            return {
+                filters: {
+                    company: frm.doc.company
+                }
+            }
+        })
+        
+        frm.set_query("account", "accounts", function() {
+            return {
+                filters: {
+                    company: frm.doc.company,
+                    report_type: "Profit and Loss",
+                    is_group: 0
+                }
+            }
+        })
+        
+        frm.set_query("monthly_distribution", function() {
+            return {
+                filters: {
+                    fiscal_year: frm.doc.fiscal_year
+                }
+            }
+        })
+    },
 
-	refresh: function(frm) {
-		frm.trigger("toggle_reqd_fields")
-	},
+    refresh: function(frm) {
+        frm.trigger("toggle_reqd_fields")
+    },
 
-	budget_against: function(frm) {
-		frm.trigger("set_null_value")
-		frm.trigger("toggle_reqd_fields")
-	},
+    budget_against: function(frm) {
+        frm.trigger("set_null_value")
+        frm.trigger("toggle_reqd_fields")
+    },
 
-	set_null_value: function(frm) {
-		if(frm.doc.budget_against == 'Cost Center') {
-			frm.set_value('project', null)
-		} else {
-			frm.set_value('cost_center', null)
-		}
-	},
+    set_null_value: function(frm) {
+        if(frm.doc.budget_against == 'Cost Center') {
+            frm.set_value('project', null)
+        } else {
+            frm.set_value('cost_center', null)
+        }
+    },
 
-	toggle_reqd_fields: function(frm) {
-		frm.toggle_reqd("cost_center", frm.doc.budget_against=="Cost Center");
-		frm.toggle_reqd("project", frm.doc.budget_against=="Project");
-	}
+    toggle_reqd_fields: function(frm) {
+        frm.toggle_reqd("cost_center", frm.doc.budget_against=="Cost Center");
+        frm.toggle_reqd("project", frm.doc.budget_against=="Project");
+    }
 });
+
+frappe.ui.form.on('Budget Account', {
+    budget_amount(frm, cdt, cdn) {
+        update_total(frm);
+    }
+});
+
+function update_total(frm) {
+    let total = 0;
+    if (frm.doc.accounts) {
+        for (let i = 0; i < frm.doc.accounts.length; i++) {
+            total += frm.doc.accounts[i].budget_amount;
+        }
+    }
+    cur_frm.set_value("total", total);
+}
--- erpnext/accounts/doctype/monthly_distribution/monthly_distribution.js
+++ erpnext/accounts/doctype/monthly_distribution/monthly_distribution.js
@@ -1,16 +1,32 @@
-// Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and Contributors
+// Copyright (c) 2015-2025, libracore and Contributors
 // License: GNU General Public License v3. See license.txt
 
 frappe.ui.form.on('Monthly Distribution', {
-	onload(frm) {
-		if(frm.doc.__islocal) {
-			return frm.call('get_months').then(() => {
-				frm.refresh_field('percentages');
-			});
-		}
-	},
+    onload(frm) {
+        if(frm.doc.__islocal) {
+            return frm.call('get_months').then(() => {
+                frm.refresh_field('percentages');
+            });
+        }
+    },
 
-	refresh(frm) {
-		frm.toggle_display('distribution_id', frm.doc.__islocal);
-	}
+    refresh(frm) {
+        frm.toggle_display('distribution_id', frm.doc.__islocal);
+    }
 });
+
+frappe.ui.form.on('Monthly Distribution Percentage', {
+    percentage_allocation(frm, cdt, cdn) {
+        update_total(frm);
+    }
+});
+
+function update_total(frm) {
+    let total = 0;
+    if (frm.doc.percentages) {
+        for (let i = 0; i < frm.doc.percentages.length; i++) {
+            total += frm.doc.percentages[i].percentage_allocation;
+        }
+    }
+    cur_frm.set_value("total", total);
+}
