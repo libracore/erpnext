d455bc6edc1230406507c13b846f854ce645eb60
Martin Zoller <martin.zoller@senstech.ch>
2025-02-21 10:22:55 +0100

    Fix timesheet end time calc, see https://github.com/frappe/erpnext/issues/20848

--- erpnext/projects/doctype/timesheet/timesheet.js
+++ erpnext/projects/doctype/timesheet/timesheet.js
@@ -162,19 +162,11 @@ frappe.ui.form.on("Timesheet Detail", {
 
 	to_time: function(frm, cdt, cdn) {
 		var child = locals[cdt][cdn];
-		var time_diff = (moment(child.to_time).diff(moment(child.from_time),"seconds")) / ( 60 * 60 * 24);
-		var std_working_hours = 0;
 
 		if(frm._setting_hours) return;
 
 		var hours = moment(child.to_time).diff(moment(child.from_time), "seconds") / 3600;
-		std_working_hours = time_diff * frappe.working_hours;
-
-		if (std_working_hours < hours && std_working_hours > 0) {
-			frappe.model.set_value(cdt, cdn, "hours", std_working_hours);
-		} else {
-			frappe.model.set_value(cdt, cdn, "hours", hours);
-		}
+		frappe.model.set_value(cdt, cdn, "hours", hours);
 	},
 
 	time_logs_add: function(frm) {
