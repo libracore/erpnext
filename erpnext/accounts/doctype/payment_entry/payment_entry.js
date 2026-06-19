8e81be6927aa9fcc05fd12cc228f287f484fad9f
Lars <lars.mueller@libracore.com>
2026-06-19 19:48:10 +0000

    Clean payment entry from duning (ill-functional payment reminder)

--- erpnext/accounts/doctype/payment_entry/payment_entry.js
+++ erpnext/accounts/doctype/payment_entry/payment_entry.js
@@ -145,7 +145,7 @@ frappe.ui.form.on("Payment Entry", {
 		frm.set_query("reference_doctype", "references", function () {
 			let doctypes = ["Journal Entry"];
 			if (frm.doc.party_type == "Customer") {
-				doctypes = ["Sales Order", "Sales Invoice", "Journal Entry", "Dunning"];
+				doctypes = ["Sales Order", "Sales Invoice", "Journal Entry"];
 			} else if (frm.doc.party_type == "Supplier") {
 				doctypes = ["Purchase Order", "Purchase Invoice", "Journal Entry"];
 			}
