62c8b241b85128c9713bf2c9ab8367f281b3647a
lasalesi <lars.mueller@libracore.com>
2026-07-14 17:47:13 +0200

    do NOT accumulate profit and loss by default (or at all)

--- erpnext/accounts/report/profit_and_loss_statement/profit_and_loss_statement.js
+++ erpnext/accounts/report/profit_and_loss_statement/profit_and_loss_statement.js
@@ -1,4 +1,4 @@
-// Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and Contributors
+// Copyright (c) 2015-2026, libracore, Frappe Technologies Pvt. Ltd. and Contributors
 // License: GNU General Public License v3. See license.txt
 
 frappe.query_reports["Profit and Loss Statement"] = $.extend({}, erpnext.financial_statements);
@@ -22,7 +22,7 @@ frappe.query_reports["Profit and Loss Statement"]["filters"].push({
 	fieldname: "accumulated_values",
 	label: __("Accumulated Values"),
 	fieldtype: "Check",
-	default: 1,
+	default: 0,                                                         // LaMu: important: do NOT accumalate P&L!
 });
 
 frappe.query_reports["Profit and Loss Statement"]["filters"].push({
